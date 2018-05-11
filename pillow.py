a314bf3a62a35b2a8e02df75550f22a39c6bf2be
Matheus R <matheusrf96@gmail.com>
2018-05-10 21:50:12 -0300

    Primeiro commit

---
+++ pillow.py
@@ -0,0 +1,30 @@
+#coding: utf-8
+
+from PIL import Image
+import os
+
+# img = Image.open("migos.jpg")
+
+# img = img.rotate(180)
+# img.save("migos-ate-de-cabeca-para-baixo.jpg")
+
+# img.show()
+
+def novaImagem(nome):
+    tamanho = (400, 400)
+
+    imagem = Image.open(nome)
+    nova = imagem.convert("L")
+    nova = nova.resize(tamanho)
+
+    nova.save("NOVA-"+nome)
+
+    nova.show()
+
+directory = "/var/www/html/img/"
+
+for filename in os.listdir(directory):
+    if filename.endswith(".jpg"): 
+        #print(os.path.join(directory, filename))     
+        print(os.path.join(filename))     
+        novaImagem(os.path.join(filename))
